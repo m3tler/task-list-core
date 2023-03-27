b9903dcd298d165f7b54d0413dd28a368cda2552
m3tler <dawid.metlerski@outlook.com>
2023-03-27 15:40:44 +0200

    Modify test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,11 +3,11 @@ INSERT INTO users (first_name, last_name, email, password, role) VALUES ('Dawid'
 
 INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Diam id ornare imperdiet', true, '3/25/2023', 1);
 INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Lectus suspendisse', false, '3/24/2023', 1);
-INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Sapien non mi integer', true, '3/27/2023', 1);
-INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Bibendum morbi', true, '3/21/2023', 1);
-INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Ultrices posuere cubilia curae', false, '3/25/2023', 1);
+INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Sapien non mi integer', false, '3/27/2023', 1);
+INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Bibendum morbi', false, '3/31/2023', 1);
+INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Ultrices posuere cubilia curae', false, '3/30/2023', 1);
 INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Donec semper sapien a', false, '3/26/2023', 2);
-INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Amet consectetuer adipiscing', true, '3/30/2023', 2);
-INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Volutpat quam pede', false, '3/24/2023', 2);
-INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Platea dictumst aliquam augue', false, '3/26/2023', 2);
+INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Amet consectetuer adipiscing', false, '3/30/2023', 2);
+INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Volutpat quam pede', false, '3/29/2023', 2);
+INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Platea dictumst aliquam augue', true, '3/26/2023', 2);
 INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Libero ut', true, '3/31/2023', 2);
