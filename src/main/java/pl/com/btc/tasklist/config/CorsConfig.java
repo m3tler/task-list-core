e77e46ad4c0cd864c7479dffefc5145ca48139e0
m3tler <dawid.metlerski@outlook.com>
2023-03-29 09:25:09 +0200

    Update date format in test data

--- src/main/java/pl/com/btc/tasklist/config/CorsConfig.java
+++ src/main/java/pl/com/btc/tasklist/config/CorsConfig.java
@@ -9,6 +9,6 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedOrigins("http://localhost:8081").allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
 INSERT INTO users (first_name, last_name, email, password, role) VALUES ('Dawid', 'Metlerski', 'admin', '$2a$12$AnWZ9o2Wu01I8II40hNyA.aSprKK3jRtYg80xz1NQrqFcQiDEyz1u', 'ADMIN');
 INSERT INTO users (first_name, last_name, email, password, role) VALUES ('Dawid', 'Metlerski', 'user', '$2a$12$AnWZ9o2Wu01I8II40hNyA.aSprKK3jRtYg80xz1NQrqFcQiDEyz1u', 'ADMIN');
 
-INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Diam id ornare imperdiet', true, '3/25/2023', 1);
-INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Lectus suspendisse', false, '3/24/2023', 1);
-INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Sapien non mi integer', false, '3/27/2023', 1);
-INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Bibendum morbi', false, '3/31/2023', 1);
-INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Ultrices posuere cubilia curae', false, '3/30/2023', 1);
-INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Donec semper sapien a', false, '3/26/2023', 2);
-INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Amet consectetuer adipiscing', false, '3/30/2023', 2);
-INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Volutpat quam pede', false, '3/29/2023', 2);
-INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Platea dictumst aliquam augue', true, '3/26/2023', 2);
-INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Libero ut', true, '3/31/2023', 2);
+INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Diam id ornare imperdiet', true, '2023-03-25', 1);
+INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Lectus suspendisse', false, '2023-03-24', 1);
+INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Sapien non mi integer', false, '2023-03-27', 1);
+INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Bibendum morbi', false, '2023-04-20', 1);
+INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Ultrices posuere cubilia curae', false, '2023-04-30', 1);
+INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Donec semper sapien a', false, '2023-03-26', 2);
+INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Amet consectetuer adipiscing', false, '2023-04-30', 2);
+INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Volutpat quam pede', false, '2023-04-29', 2);
+INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Platea dictumst aliquam augue', true, '2023-03-26', 2);
+INSERT INTO tasks (name, is_done, deadline, user_id) VALUES ('Libero ut', true, '2023-04-21', 2);
