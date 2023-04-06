cd9c0bb0fc5e5c24f2da06ba35d204cd8bb1314b
m3tler <dawid.metlerski@outlook.com>
2023-04-06 20:06:51 +0200

    Add CORS configuration

--- src/main/java/pl/com/btc/tasklist/config/CorsConfig.java
+++ src/main/java/pl/com/btc/tasklist/config/CorsConfig.java
@@ -1,15 +1,24 @@
 package pl.com.btc.tasklist.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@AllArgsConstructor
 public class CorsConfig implements WebMvcConfigurer {
+
+    private final CorsProperties corsProperties;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173").allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedOrigins(corsProperties.getAllowedOrigins())
+                .allowedHeaders(corsProperties.getAllowedHeaders())
+                .allowedMethods(corsProperties.getAllowedMethods())
+                .exposedHeaders(corsProperties.getExposedHeaders())
+                .maxAge(corsProperties.getMaxAge())
+                .allowCredentials(corsProperties.isAllowCredentials());
     }
 }
---
+++ src/main/java/pl/com/btc/tasklist/config/CorsProperties.java
@@ -0,0 +1,63 @@
+package pl.com.btc.tasklist.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("app.cors")
+public class CorsProperties {
+    private String allowedOrigins;
+    private String allowedHeaders;
+    private String[] allowedMethods;
+    private String exposedHeaders;
+    private int maxAge;
+    private boolean allowCredentials;
+
+    public String getAllowedOrigins() {
+        return allowedOrigins;
+    }
+
+    public void setAllowedOrigins(String allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+
+    public String getAllowedHeaders() {
+        return allowedHeaders;
+    }
+
+    public void setAllowedHeaders(String allowedHeaders) {
+        this.allowedHeaders = allowedHeaders;
+    }
+
+    public String[] getAllowedMethods() {
+        return allowedMethods;
+    }
+
+    public void setAllowedMethods(String[] allowedMethods) {
+        this.allowedMethods = allowedMethods;
+    }
+
+    public String getExposedHeaders() {
+        return exposedHeaders;
+    }
+
+    public void setExposedHeaders(String exposedHeaders) {
+        this.exposedHeaders = exposedHeaders;
+    }
+
+    public int getMaxAge() {
+        return maxAge;
+    }
+
+    public void setMaxAge(int maxAge) {
+        this.maxAge = maxAge;
+    }
+
+    public boolean isAllowCredentials() {
+        return allowCredentials;
+    }
+
+    public void setAllowCredentials(boolean allowCredentials) {
+        this.allowCredentials = allowCredentials;
+    }
+}
